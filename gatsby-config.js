4169458422efa435de458259c2462a26f61bc110
cws <cws@home.fritz.box>
2020-08-13 00:52:42 +0200

    update site

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Joost Jansky`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    siteUrl: `https://reverent-feynman-270efd.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `jamifyjs`,
     },
   },
   plugins: [
