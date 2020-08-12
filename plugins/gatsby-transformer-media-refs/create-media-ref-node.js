1d8069edb9d0a307dc807d7fd4981fab879eb491
cws <cws@home.fritz.box>
2020-08-13 00:49:10 +0200

    easy fix

---
+++ plugins/gatsby-transformer-media-refs/create-media-ref-node.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const path = require(`path`);
+
+exports.createMediaRefNode = async (url, createNodeId, pluginOptions = {}) => {
+  const {
+    origin,
+    pathname
+  } = new URL(url);
+  const {
+    name,
+    ext
+  } = path.parse(pathname);
+  const mediaRefData = {
+    url,
+    origin,
+    path: pathname,
+    name,
+    ext
+  }; // Stringify date objects.
+
+  return JSON.parse(JSON.stringify({
+    id: createNodeId(url),
+    children: [],
+    parent: null,
+    internal: {},
+    sourceInstanceName: pluginOptions.name || `__PROGRAMMATIC__`,
+    ...mediaRefData
+  }));
+};
---
+++ plugins/gatsby-transformer-media-refs/create-schema-customization.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const typeDefs = `
+  type MediaRef implements Node @infer {
+    id: String
+    url: String
+    name: String
+    ext: String
+    extension: String
+    mediaType: String
+    origin: String
+    path: String
+    sourceInstanceName: String
+    imageFormat: String
+    imageHeight: Int
+    imageWidth: Int
+    targetURL: String
+  }
+`;
+
+const createSchemaCustomization = ({
+  actions
+}) => {
+  const {
+    createTypes
+  } = actions;
+  createTypes(typeDefs);
+};
+
+module.exports = createSchemaCustomization;
